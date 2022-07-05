0a8d3953cbf6ffadcca1a14a276d40648c161223
maulana6008 <adamherry123456@gmail.com>
2022-07-05 19:55:19 +0700

    change redirect from index.php to dashboard

--- logics/login.php
+++ logics/login.php
@@ -6,7 +6,7 @@
         $login_check = $koneksi->query("SELECT * FROM pegawai WHERE id_pegawai='$id' ");
         if($login_check->num_rows){
             echo "<script>alert('You have session')</script>";
-            echo "<script>location='index.php'</script>";
+            echo "<script>location='dashboard'</script>";
         }
     }
 
@@ -19,7 +19,7 @@
             if($check->num_rows >= 1){
                 $_SESSION['user'] = $check->fetch_object()->id_pegawai;
                 echo "<script>alert('Login Successfuly')</script>";
-                echo "<script>location='index.php'</script>";
+                echo "<script>location='dashboard'</script>";
             }
         }else{
             echo "<script>alert('Your Password is Wrong')</script>";
